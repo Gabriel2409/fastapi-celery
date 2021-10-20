53588b3f2fb600278f941978372ca8b1fc78ee83
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-10-20 08:41:00 +0200

    modified config celery

--- project/app/main.py
+++ project/app/main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI
 
 def create_celery():
     celery_app = current_celery_app
-    celery_app.config_from_object(settings, namespace="CELERY")
+    celery_app.config_from_object(settings.CELERY_CONFIG)
 
     return celery_app
 
